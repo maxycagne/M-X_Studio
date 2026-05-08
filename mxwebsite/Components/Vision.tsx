55293415447403ccec3e75a42066b6937809f8dd
Maxy <maverickcagne@gmail.com>
2026-05-08 19:56:24 +0800

    remove comment

--- mxwebsite/Components/Vision.tsx
+++ mxwebsite/Components/Vision.tsx
@@ -3,7 +3,6 @@ import React from "react";
 const Vision = () => {
   return (
     <section className="relative group">
-      {/* Glow positioned on the right to balance with the Mission component */}
       <div className="absolute -right-10 top-0 w-32 h-32 bg-[#000051] rounded-full blur-[50px] pointer-events-none" />
 
       <h2 className="text-4xl md:text-6xl font-black tracking-tighter mb-8 leading-tight">
